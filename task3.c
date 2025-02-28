42243b192e3fda1f8ee5ab7b4e2a63021aaa1170
RSMT98 <robsmt@mail.ru>
2025-03-01 02:21:21 +0300

    Done

---
+++ task3.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define BUFFER_SIZE 5120
+
+int main(int argc, char *argv[])
+{
+    int pipefd[2];
+    if (pipe(pipefd) < 0)
+    {
+        perror("Error creating a pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t pid = fork();
+    switch (pid)
+    {
+    case -1:
+        perror("Error creating a child process");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        exit(EXIT_FAILURE);
+    case 0:
+        if (close(pipefd[1]) < 0)
+        {
+            perror("Child: Error closing the write end of the pipe");
+            close(pipefd[0]);
+            exit(EXIT_FAILURE);
+        }
+
+        char buffer[BUFFER_SIZE];
+        ssize_t bytes_read = 0;
+
+        while ((bytes_read = read(pipefd[0], buffer, sizeof(buffer))) > 0)
+        {
+            ssize_t bytes_written = 0;
+
+            while (bytes_written < bytes_read)
+            {
+                ssize_t bytes_written_out = write(STDOUT_FILENO, buffer + bytes_written, bytes_read - bytes_written);
+                if (bytes_written_out <= 0)
+                {
+                    perror("Child: Write error");
+                    close(pipefd[0]);
+                    exit(EXIT_FAILURE);
+                }
+                bytes_written += bytes_written_out;
+            }
+        }
+
+        if (bytes_read == -1)
+        {
+            perror("Child: Read error");
+            close(pipefd[0]);
+            exit(EXIT_FAILURE);
+        }
+
+        if (close(pipefd[0]) < 0)
+        {
+            perror("Child: Error closing the read end of the pipe");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    default:
+        if (close(pipefd[0]) < 0)
+        {
+            perror("Parent: Error closing the read end of the pipe");
+            close(pipefd[1]);
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < argc; ++i)
+        {
+            const char *arg = argv[i];
+            size_t arg_length = strlen(arg);
+            size_t bytes_written = 0;
+
+            while (bytes_written < arg_length)
+            {
+                size_t bytes_transfered = write(pipefd[1], arg + bytes_written, arg_length - bytes_written);
+                if (bytes_transfered <= 0)
+                {
+                    perror("Parent: Write error");
+                    close(pipefd[1]);
+                    exit(EXIT_FAILURE);
+                }
+                bytes_written += bytes_transfered;
+            }
+            if (write(pipefd[1], "\n", 1) <= 0)
+            {
+                perror("Parent: Error writing the '\\n'");
+                close(pipefd[1]);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        int status;
+        if (close(pipefd[1]) < 0)
+        {
+            perror("Parent: Error closing the write end of the pipe");
+            exit(EXIT_FAILURE);
+        }
+        if (waitpid(pid, &status, 0) < 0)
+        {
+            perror("Parent: Error waiting for a child process to exit");
+            exit(EXIT_FAILURE);
+        }
+
+        if (WIFEXITED(status))
+            exit(WEXITSTATUS(status));
+        else
+        {
+            perror("Some untraceable error occurred in a child process");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
---
+++ user/task2.c
@@ -0,0 +1,102 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int pipefd[2];
+    if (pipe(pipefd) < 0)
+    {
+        fprintf(2, "Error creating a pipe\n");
+        exit(1);
+    }
+
+    int pid = fork();
+    switch (pid)
+    {
+    case -1:
+        fprintf(2, "Error creating a child process\n");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        exit(1);
+    case 0:
+        if (close(pipefd[1]) < 0)
+        {
+            fprintf(2, "Child: Error closing the write end of the pipe\n");
+            close(pipefd[0]);
+            exit(1);
+        }
+        if (close(0) < 0)
+        {
+            fprintf(2, "Child: Error closing stdin\n");
+            close(pipefd[0]);
+            exit(1);
+        }
+        if (dup(pipefd[0]) < 0)
+        {
+            fprintf(2, "Child: Error replacing stdin with the read end of the pipe\n");
+            close(pipefd[0]);
+            exit(1);
+        }
+        if (close(pipefd[0]) < 0)
+        {
+            fprintf(2, "Child: Error closing the read end of the pipe\n");
+            exit(1);
+        }
+
+        char *wc_argv[] = {"/wc", 0};
+        if (exec(wc_argv[0], wc_argv) < 1)
+        {
+            fprintf(2, "Child: Error calling the exec\n");
+            exit(1);
+        }
+
+        fprintf(2, "Child: exec failed\n");
+        exit(1);
+    default:
+        if (close(pipefd[0]) < 0)
+        {
+            fprintf(2, "Parent: Error closing the read end of the pipe\n");
+            close(pipefd[1]);
+            exit(1);
+        }
+
+        for (int i = 0; i < argc; ++i)
+        {
+            char *arg = argv[i];
+            int arg_length = strlen(arg);
+            int bytes_written = 0;
+
+            while (bytes_written < arg_length)
+            {
+                int bytes_transfered = write(pipefd[1], arg + bytes_written, arg_length - bytes_written);
+                if (bytes_transfered <= 0)
+                {
+                    fprintf(2, "Parent: Write error\n");
+                    close(pipefd[1]);
+                    exit(1);
+                }
+                bytes_written += bytes_transfered;
+            }
+            if ((i != (argc - 1)) && (write(pipefd[1], "\n", 1) <= 0))
+            {
+                fprintf(2, "Parent: Error writing the '\\n'\n");
+                close(pipefd[1]);
+                exit(1);
+            }
+        }
+
+        int status;
+        if (close(pipefd[1]) < 0)
+        {
+            fprintf(2, "Parent: Error closing the write end of the pipe\n");
+            exit(1);
+        }
+        if (wait(&status) < 0)
+        {
+            fprintf(2, "Parent: Error waiting for a child process to exit\n");
+            exit(1);
+        }
+        exit(0);
+    }
+}
